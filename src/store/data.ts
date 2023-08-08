034330506d832305b7956a3852b0d24f2491a391
jacky <jackywu526@gmail.com>
2023-08-08 12:00:15 +0800

    feat: init

---
+++ src/store/data.ts
@@ -0,0 +1,196 @@
+const classRoomKey = [
+  "麥塊初階班",
+  "麥塊進階班",
+  "麥塊綜合應用班"
+];
+
+const lesson = {
+  "麥塊初階班": [
+    {
+      key: "L1",
+      content: "認識麥塊"
+    },
+    {
+      key: "L2",
+      content: "認識 Agent"
+    },
+    {
+      key: "L3",
+      content: "螺旋樓梯"
+    },
+    {
+      key: "L4",
+      content: "建造高塔"
+    },
+    {
+      key: "L5",
+      content: "引爆 TNT"
+    },
+    {
+      key: "L6",
+      content: "農夫 Agent"
+    },
+    {
+      key: "L7",
+      content: "發射煙火"
+    },
+    {
+      key: "L8",
+      content: "鵝卵石生產線"
+    },
+    {
+      key: "L9",
+      content: "TNT 大砲"
+    },
+    {
+      key: "L10",
+      content: "火焰發射器"
+    },
+    {
+      key: "L11",
+      content: "紅石電路"
+    },
+    {
+      key: "L12",
+      content: "自動販賣機"
+    },
+    {
+      key: "L13",
+      content: "自動化生產線"
+    },
+    {
+      key: "L14",
+      content: "雲霄飛車"
+    },
+    {
+      key: "L15",
+      content: "自動化農作物收割機"
+    },
+  ],
+  "麥塊進階班": [
+    {
+      key: "L1",
+      content: "觀察者自動農場"
+    },
+    {
+      key: "L2",
+      content: "金字塔守衛"
+    },
+    {
+      key: "L3",
+      content: "美妙音符方塊"
+    },
+    {
+      key: "L4",
+      content: "智能大考驗"
+    },
+    {
+      key: "L5",
+      content: "藥水魔法師"
+    },
+    {
+      key: "L6",
+      content: "麥田圈印表機"
+    },
+    {
+      key: "L7",
+      content: "狩獵遊戲"
+    },
+    {
+      key: "L8",
+      content: "超人換裝間"
+    },
+    {
+      key: "L9",
+      content: "猜拳遊戲"
+    },
+    {
+      key: "L10",
+      content: "彩色隱藏門"
+    },
+    {
+      key: "L11",
+      content: "秘密圖書館"
+    },
+    {
+      key: "L12",
+      content: "解謎達人"
+    },
+    {
+      key: "L13",
+      content: "快速傳輸器"
+    },
+    {
+      key: "L14",
+      content: "計時跑酷賽"
+    },
+    {
+      key: "L15",
+      content: "成功破關王"
+    }
+  ],
+  "麥塊綜合應用班": [
+    {
+      key: "L1",
+      content: "攤販製造機"
+    },
+    {
+      key: "L2",
+      content: "中央噴水池"
+    },
+    {
+      key: "L3",
+      content: "遊戲攤販"
+    },
+    {
+      key: "L4",
+      content: "壽司攤販"
+    },
+    {
+      key: "L5",
+      content: "商店街導覽員"
+    },
+    {
+      key: "L6",
+      content: "射箭運動會"
+    },
+    {
+      key: "L7",
+      content: "超級障礙賽"
+    },
+    {
+      key: "L8",
+      content: "怪物闖關賽"
+    },
+    {
+      key: "L9",
+      content: "幸運跑酷賽"
+    },
+    {
+      key: "L10",
+      content: "急速礦車手"
+    },
+    {
+      key: "L11",
+      content: "雙人大富翁"
+    },
+    {
+      key: "L12",
+      content: "機會與命運"
+    },
+    {
+      key: "L13",
+      content: "道具商店"
+    },
+    {
+      key: "L14",
+      content: "星星兌換站"
+    },
+    {
+      key: "L15",
+      content: "大富翁機制"
+    }
+  ]
+};
+
+export { classRoomKey, lesson };
