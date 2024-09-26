901515f4302990f07c77145a35b39965a6de5b21
jacky <jackywu526@gmail.com>
2024-09-26 09:50:59 +0800

    refactor: 整體畫面重構

---
+++ src/view/OperateView.vue
@@ -0,0 +1,20 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <!-- 學生輸入區 -->
+    <div>
+      輸入區
+    </div>
+    <!-- 動腦時間輸入區 -->
+    <div>
+      動腦時間輸入區
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
