e871a7bf5f7e35a03f542f23ae383f4b000ee73c
jacky <jackywu526@gmail.com>
2024-10-01 23:42:12 +0800

    feat(extend): install uuid / fontawesome

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,20 @@ import './style/style.css';
 import 'element-plus/dist/index.css';
 import './style/tailwindCSS.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faArrowsDownToLine, faArrowsTurnRight } from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
+
+/* add icons to the library */
+library.add(faArrowsDownToLine, faArrowsTurnRight);
+
 // Create a Pinia instance
 const pinia = createPinia();
 
@@ -17,4 +30,5 @@ Object.entries(ElementPlusIconsVue).forEach(([key, component]) => {
 
 app.use(ElementPlus);
 app.use(pinia);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
