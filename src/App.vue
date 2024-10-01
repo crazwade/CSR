488bfe94f1e60994857934d7576b40baf9a20c5e
jacky <jackywu526@gmail.com>
2024-10-01 23:43:28 +0800

    style(App.vue): adjust height seating and operate area

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="h-full w-full bg-pink-200 p-2 flex flex-col">
     <!-- 座位區 -->
-    <div class="w-full h-4/5 bg-blue-100">
+    <div class="w-full h-fit max-h-[800px] bg-blue-100 overflow-auto">
       <SeatingView />
     </div>
     <!-- 操作區 -->
-    <div class="w-full h-1/5 bg-green-100">
+    <div class="w-full h-fit bg-green-100">
       <OperateView />
     </div>
   </div>
