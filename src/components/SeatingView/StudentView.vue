da0ee41740005e6a4f725909c3c311db7db3a57a
jacky <jackywu526@gmail.com>
2024-10-03 00:29:56 +0800

    feat(StudentView.vue): 製作學生區的畫面組件

---
+++ src/components/SeatingView/StudentView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { Student } from '../../store/dataStore';
+
+defineProps<{
+  data: Student[];
+}>();
+
+const emit = defineEmits<{
+  dragStudent: [id: string];
+  close: [id: string];
+}>();
+
+const onDragStudent = (e: DragEvent): void => {
+  e.preventDefault();
+
+  if (e.target) {
+    emit('dragStudent', (e.target as HTMLElement).id);
+  }
+};
+</script>
+
+<template>
+  <el-tag
+    v-for="student in data"
+    :key="student.id"
+    :id="`${student.id}`"
+    v-on:drag="onDragStudent"
+    @close="emit('close', student.id)"
+    :effect="'dark'"
+    :size="'large'"
+    :type="'warning'"
+    :draggable="true"
+    closable
+  >
+    {{ student.name }}
+  </el-tag>
+</template>
+
+<style lang="scss" scoped></style>
