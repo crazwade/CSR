060384ea42f1f79cc8dc000ac7b3209b8b6c0604
jacky <jackywu526@gmail.com>
2024-10-03 19:18:26 +0800

    feat(StudentView.vue): 將學生姓名區移至 OperateView

--- src/view/OperateView.vue
+++ src/view/OperateView.vue
@@ -1,20 +1,24 @@
-<script setup lang='ts'>
+<script setup lang="ts">
+import { useDataStore } from '../store/dataStore.ts';
+import StudentView from '../components/OperateView/StudentView.vue';
 
+const dataStore = useDataStore();
 </script>
 
 <template>
   <div class="flex flex-col">
     <!-- 學生輸入區 -->
-    <div>
-      輸入區
+    <div>輸入區</div>
+    <div class="w-full max-w-full h-fit flex gap-2 flex-wrap">
+      <StudentView
+        @dragStudent="(id) => dataStore.setDragInfo({ studentId: id })"
+        @close="(id) => dataStore.removeStudent(id)"
+        :data="dataStore.getStudentListNotInSeat"
+      />
     </div>
     <!-- 動腦時間輸入區 -->
-    <div>
-      動腦時間輸入區
-    </div>
+    <div>動腦時間輸入區</div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
