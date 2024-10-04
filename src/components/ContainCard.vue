1d081f21330ead9dfdf31c98dd1a271bd6a79908
jacky <jackywu526@gmail.com>
2024-10-04 17:03:25 +0800

    feat(ContainCard): 操作區的畫面容器組件

---
+++ src/components/ContainCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string;
+    width?: 'full' | 'fit';
+  }>(),
+  {
+    width: 'fit',
+  }
+);
+</script>
+
+<template>
+  <div
+    class="flex flex-col justify-start gap-3"
+    :class="[width === 'fit' ? 'w-fit' : 'w-full']"
+  >
+    <h1 v-if="title" class="text-xl font-bold">{{ title }}</h1>
+    <div class="rounded-3xl bg-white p-3">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
