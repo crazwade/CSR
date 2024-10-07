f3b365580e32c247a646faa477435afcb2154956
jacky <jackywu526@gmail.com>
2024-10-07 17:46:41 +0800

    style(main.ts): 調整 css 引入順序避免 element-plus 與 tailwindcss 衝突

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
 import './style/style.css';
-import 'element-plus/dist/index.css';
 import './style/tailwindCSS.css';
+import 'element-plus/dist/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 /* import the fontawesome core */
@@ -14,12 +14,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 /* import specific icons */
 import {
-  faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faUserXmark, faChair, faUpload,
+  faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faUserXmark, faChair, faUpload, faCopy
 } from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
 
 /* add icons to the library */
-library.add(faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faUserXmark, faChair, faUpload);
+library.add(faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faUserXmark, faChair, faUpload, faCopy);
 
 // Create a Pinia instance
 const pinia = createPinia();
