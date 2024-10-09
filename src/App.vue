589cda4af023d4bb464bff03b6e1344f90c4a585
jacky <jackywu526@gmail.com>
2024-10-09 14:49:14 +0800

    feat(App.vue): 調整畫面並添加 version

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import OperateView from './view/OperateView.vue';
+import SeatingView from './view/SeatingView.vue';
+import { version } from '../package.json';
+</script>
+
 <template>
   <div class="h-full w-full flex flex-col">
     <!-- 座位區 -->
@@ -9,12 +15,10 @@
       <OperateView />
     </div>
   </div>
+  <div class="absolute top-0 right-1 text-sm text-gray-400">
+    {{ version }}
+  </div>
 </template>
-
-<script setup lang="ts">
-import OperateView from './view/OperateView.vue';
-import SeatingView from './view/SeatingView.vue';
-</script>
 
 <style scoped>
 .spec-bg {
